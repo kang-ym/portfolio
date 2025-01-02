640286022ab15cb24af0e768159f7c77b483b86f
kang-ym <mpspx@naver.com>
2025-01-03 00:05:29 +0900

    JS-projects-work-funcionname

--- src/projects.js
+++ src/projects.js
@@ -9,22 +9,31 @@ categories.addEventListener('click', (event) => {
     if (filter == null) {
         return;
     }
-    // Active 메뉴를 재설정
-    const active = document.querySelector('.category--selected');
-    active.classList.remove('category--selected');
-    event.target.classList.add('category--selected');
-    
-    //프로젝트 필터링 
-    projectsContainer.classList.add('anim-out');
-    projects.forEach((project) => { // 순회하면서 프로젝트당 한가지씩 일 처리
-        if (filter === 'all' || filter === project.dataset.type) {
-            project.style.display = 'block'; // 보여주고
-        } else {
-            project.style.display = 'none'; // 보여주지 않고
-        }
-    });
-    setTimeout(()=>{
-        projectsContainer.classList.remove('anim-out');
-
-    }, 250);
+    handleActiveSelection(event.target);
+    filterProjects(filter);
 });
+
+// 코드가 너무 많다면 함수로 정리해주면 좋다 / 그러면 주석으로 이름 정리를 안해도 되고 알아보기 편하다
+function handleActiveSelection(target){
+        // Active 메뉴를 재설정
+        const active = document.querySelector('.category--selected');
+        active.classList.remove('category--selected');
+        target.classList.add('category--selected');
+    
+};
+function filterProjects(filter){
+        //프로젝트 필터링 
+        projectsContainer.classList.add('anim-out');
+        projects.forEach((project) => { // 순회하면서 프로젝트당 한가지씩 일 처리
+            if (filter === 'all' || filter === project.dataset.type) {
+                project.style.display = 'block'; // 보여주고
+            } else {
+                project.style.display = 'none'; // 보여주지 않고
+            }
+        });
+        setTimeout(()=>{
+            projectsContainer.classList.remove('anim-out');
+    
+        }, 250);
+    
+};
